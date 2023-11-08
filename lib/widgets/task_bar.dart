55572f3e7b3da0eef87e84f5962b3592816ebd33
Nitram13E <martinquot@gmail.com>
2023-11-08 09:30:38 -0300

    Cambios TaskBar

--- lib/widgets/task_bar.dart
+++ lib/widgets/task_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Taskbar extends StatefulWidget {
   const Taskbar({super.key});
@@ -8,6 +9,9 @@ class Taskbar extends StatefulWidget {
 }
 
 class _TaskbarState extends State<Taskbar> {
+  TextEditingController timeTextEditingController = TextEditingController();
+  TextEditingController issueTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,11 +22,29 @@ class _TaskbarState extends State<Taskbar> {
             value: false,
             onChanged: (value) {},
           ),
-          //showTimePicker
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.timelapse_rounded),
+          ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxHeight: 40,
+              minHeight: 20,
+              maxWidth: 80,
+              minWidth: 50,
+            ),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              controller: timeTextEditingController,
+              onChanged: (value) {
+                if (value.contains(' ')) {
+                  //TODO: Definir logica de formato
+                  //timeTextEditingController.text = value.replaceRange(RegExp(source), )
+                }
+              },
+              decoration: const InputDecoration(
+                labelText: "Time",
+                border: OutlineInputBorder(),
+              ),
+            ),
           ),
+          const Text(' - '),
           ConstrainedBox(
             constraints: const BoxConstraints(
               maxHeight: 40,
